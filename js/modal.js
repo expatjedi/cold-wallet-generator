d8f7885ec5f308f909a5f5e2a6c801c95ba8e89b
Koray Biçer <kbicer@outlook.com.tr>
2021-05-28 14:04:43 +0300

    modal css

---
+++ js/modal.js
@@ -0,0 +1,1 @@
+var modal=document.getElementById("myModal"),btn=document.getElementById("myBtn"),span=document.getElementsByClassName("close")[0];btn.onclick=function(){modal.style.display="block"};span.onclick=function(){modal.style.display="none"};window.onclick=function(a){a.target==modal&&(modal.style.display="none")};
